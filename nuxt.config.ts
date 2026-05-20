c8c7a81ac360734e320d9105f8fc7b8c2587652c
Piyakarn Nimmakulvirut <piyakarn.nmk@gmail.com>
2026-05-20 15:53:06 +0700

    feat: init project

---
+++ nuxt.config.ts
@@ -0,0 +1,15 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+
+  modules: [
+    '@nuxt/image',
+    '@nuxt/icon',
+    '@nuxt/fonts',
+    '@nuxtjs/color-mode',
+    '@nuxtjs/tailwindcss',
+    'dayjs-nuxt',
+    'nuxt-toast'
+  ]
+})
